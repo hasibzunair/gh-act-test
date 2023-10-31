50daae962be94c8fce807c893c60aab6e4579928
hasibzunair <hasibzunair@gmail.com>
2023-10-31 17:39:37 -0400

    added new function and test

--- test/test_adder.py
+++ test/test_adder.py
@@ -3,6 +3,6 @@ sys.path.insert(0, "../src/")
 from src.adder import add_numbers
 
 def test_adder():
-    res = add_numbers(5 + 5)
+    res = add_numbers(5, 5)
     # test will pass only if function returns 10
     assert res == 10, f"Should be 10, got: {res}"
