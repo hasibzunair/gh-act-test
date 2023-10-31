3cf9c81e19da358999aff24a868d8f7303d51919
hasibzunair <hasibzunair@gmail.com>
2023-10-31 17:09:21 -0400

    added test.yml

--- test/test_hello.py
+++ test/test_hello.py
@@ -2,4 +2,4 @@ from src.hello import hello
 
 def test_hello():
     res = hello()
-    assert res == "Hello World!" f"Should be Hello World!, got: {res}"
+    assert res == "Hello World!", f"Should be Hello World!, got: {res}"
