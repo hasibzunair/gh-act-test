73db970fa3422847e427f49777100f196ad15749
hasibzunair <hasibzunair@gmail.com>
2023-10-31 17:26:30 -0400

    added test.yml with push pull

--- test/test_hello.py
+++ test/test_hello.py
@@ -1,5 +1,8 @@
+import sys
+sys.path.insert(0, "../src/")
 from src.hello import hello
 
 def test_hello():
     res = hello()
+    # test will pass only if function returns "Hello World!"
     assert res == "Hello World!", f"Should be Hello World!, got: {res}"
